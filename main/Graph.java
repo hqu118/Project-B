1a95e9389f46f66e5e49c9d96a8235be61ea99cc
hqu118 <hqu118@aucklanduni.ac.nz>
2023-10-18 20:55:19 +1300

    Update Graph.java

--- main/Graph.java
+++ main/Graph.java
@@ -1,7 +1,9 @@
 package main;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Graph {
 	private HashMap<Node, EdgesLinkedList> adjacencyMap;
@@ -33,5 +35,67 @@ public class Graph {
 			}
 			i++;
 		}
+	}
+
+	
+	public boolean isNodeInGraph(Node node) {
+		if (node == null) {
+			return false;
+		}
+
+		if (adjacencyMap.containsKey(node)) {
+			return true;
+		}
+
+		return false;
+	}
+
+		/**
+	 * This method finds an edge with a specific weight, if there are more than one
+	 * you need to return the first you encounter. You must use Breath First Search
+	 * (BFS) strategy starting from the root.
+	 * <p>
+	 * You can create a data structure to keep track of the visited nodes Set<Node>
+	 * visited = new HashSet<>(); If you don't keep track of the visited nodes the
+	 * method will run forever!
+	 * <p>
+	 * <p>
+	 * In addition to the data structure visited you can only create new
+	 * datastructures of type EdgesLinkedList and NodesStackAndQue
+	 *
+	 * @param weight
+	 * @return the Edge with the specific weight, null if no edge with the specif
+	 *         weight exists in teh graph
+	 */
+	public Edge searchEdgeByWeight(int weight) {
+		if (weight < 0) {
+			return null;
+		}
+
+		Set<Node> nodeVisited = new HashSet<>();
+		NodesStackAndQueue nodesDuringSearch = new NodesStackAndQueue();// create a new stack and queue to store the nodes
+		while (!nodeVisited.containsAll(adjacencyMap.keySet())) {
+			EdgesLinkedList edgesLinkedList = adjacencyMap.get(root); // initialize the edges linked list for the root node
+			Edge edgeOfSourceNode = edgesLinkedList.get(edgesLinkedList.size() - 1); // initialize the last edge from the source node
+
+			if (!nodeVisited.contains(root)) {
+				nodeVisited.add(root);
+			}
+
+			for (int i = 0; i < edgesLinkedList.size(); i++) {
+				edgeOfSourceNode = edgesLinkedList.get(i); // edgeOfSourceNode will be the edges of the same source node
+				if (edgeOfSourceNode.getWeight() == weight) {
+					return edgeOfSourceNode;
+				} else {
+					Node targetNode = edgeOfSourceNode.getTarget();
+					if (!nodeVisited.contains(targetNode)) {
+						nodesDuringSearch.append(targetNode); //push the node into stack and queue so the last target node (if not in node visited) will be on the top and pop will return it
+					}
+				}
+			}
+			root = nodesDuringSearch.remove();
+		}
+
+		return null;
 	}
 }
