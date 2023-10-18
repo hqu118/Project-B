9b202c153fc293d1ea9190b475fdd2728a292769
qli627 <qli627@aucklanduni.ac.nz>
2023-10-18 20:15:13 +1300

    update Node.java

--- main/Node.java
+++ main/Node.java
@@ -13,32 +13,38 @@ public class Node {
 
     // set next node
     public void setNext(Node n) {
+        next = n;
     }
 
     // get next node
     public Node getNext() {
-        return null;
+        return next;
     }
 
     public String getValue() {
-        return null;
+        return value;
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o)
             return true;
-        return false;
+        if (!(o instanceof Node))
+            return false;
+        Node node = (Node) o;
+        return Objects.equals(value, node.value);
     }
 
     @Override
     public int hashCode() {
-        return 0;
+        return Objects.hash(value);
     }
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("Node{");
+        sb.append("value='").append(value).append('\'');
+        sb.append('}');
         return sb.toString();
     }
 }
