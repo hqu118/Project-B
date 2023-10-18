a3381a8d8dfb99d0e1edd7f84e0a88b7b1c76a20
hqu118 <hqu118@aucklanduni.ac.nz>
2023-10-18 20:11:06 +1300

    Update Node.java

--- main/Node.java
+++ main/Node.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Objects;
+
 public class Node {
     private String value;
     private Node next;
@@ -7,5 +9,36 @@ public class Node {
     public Node(String v) {
         value = v;
         next = null;
+    }
+
+    // set next node
+    public void setNext(Node n) {
+    }
+
+    // get next node
+    public Node getNext() {
+        return null;
+    }
+
+    public String getValue() {
+        return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Node{");
+        return sb.toString();
     }
 }
