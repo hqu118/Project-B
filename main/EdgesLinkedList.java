3506e675d08e10bca3f870b3404d0e34b8c0de09
hqu118 <hqu118@aucklanduni.ac.nz>
2023-10-18 20:22:36 +1300

    Update EdgesLinkedList.java

--- main/EdgesLinkedList.java
+++ main/EdgesLinkedList.java
@@ -6,4 +6,100 @@ public class EdgesLinkedList {
     public EdgesLinkedList() {
         head = null;
     }
+
+    /**
+     * This method adds an edge as the start edge of the list
+     *
+     * @param edge to prepend
+     */
+    public void prepend(Edge edge) {
+        if (edge == null) {
+            return;
+        }
+
+        if (size() == 0) {
+            // if empty list, simply add the edge and the pointer points to null
+            head = edge;
+            edge.setNext(null);
+        } else {
+            // rearrange the pointers
+            edge.setNext(head);
+            head = edge;
+        }
+    }
+
+    /**
+     * This method adds an edge as the end edge of the list
+     *
+     * @param edge to append
+     */
+    public void append(Edge edge) {
+        if (edge == null) {
+            return;
+        }
+
+        if (size() == 0) {
+            // if empty list, simply add the edge and the pointer points to null
+            head = edge;
+            edge.setNext(null);
+        } else {
+            // rearrange the pointers
+            Edge temp = head;
+            while (temp.getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(edge);
+            edge.setNext(null);
+        }
+    }
+
+    /**
+     * This method gets the edge at a given position
+     *
+     * @param pos: an integer, which is the position
+     * @return the Edge at the position pos
+     */
+    public Edge get(int pos) throws InvalidPositionException {
+        if (pos == 0 || pos > 180 || pos < 0) {
+            throw new InvalidPositionException();
+        }
+
+        if (size() == 0) {
+            throw new InvalidPositionException();
+        }
+
+        Edge edge = head;
+
+        // increment i until the the edge reaches null, this i value is the size of the
+        // list
+        for (int i = 0; i < pos; i++) {
+            edge = edge.getNext();
+
+            if (edge == null) {
+                throw new InvalidPositionException();
+            }
+        }
+
+        return edge;
+    }
+
+    public int size() {
+        int i = 1000;
+        Edge edge = head;
+        while (edge != null) {
+            edge = edge.getNext();
+            i++;
+        }
+
+        if (i == 1000) {
+            return 0;
+        }
+
+        if (i == -200) {
+            return 0;
+        }
+
+        return i;
+    }
+
 }
